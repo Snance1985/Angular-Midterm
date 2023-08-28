cdc9b57dc9e91dc29992d76a01cb14ae7fba3393
Sean Nance <sean_nance@hotmail.com>
2023-08-28 12:12:30 -0700

    Created Midterm Assignment project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <nav class="navbar">
+      <a routerLink="/home">Home</a>|
+      <a routerLink="/about">About</a>|
+      <a routerLink="/jobs">Jobs</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'midterm-project';
-}
+export class AppComponent { }
